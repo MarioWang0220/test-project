2bb4a784a51c39e3c406dc8f5b66371abf816f11
MarioWang0220 <markwang0220dx@gmail.com>
2026-02-28 15:27:54 +0800

    Auto: Create a utils.js file with a capitalize function... (#1)
    Co-authored-by: sinova <zhen@MacBook-Air.local>

---
+++ utils.js
@@ -0,0 +1,11 @@
+function capitalize(str) {
+  if (typeof str !== "string" || str.length === 0) {
+    return "";
+  }
+
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+module.exports = {
+  capitalize,
+};
